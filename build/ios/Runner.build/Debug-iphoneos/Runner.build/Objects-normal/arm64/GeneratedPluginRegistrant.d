6188e992519b54b4a2b098285882ebee3f2d406b
Marcelinho Lopes <marcelinholopes@MacCeloPro.local>
2020-04-26 18:16:13 -0300

    Importação inicial

---
+++ build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,19 @@
+dependencies: \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/marcelinholopes/Downloads/Cursos/Flutter/Projetos/curso_flutter/calculator/ios/Flutter/Flutter.framework/Headers/Flutter.h
